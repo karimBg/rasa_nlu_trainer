0b3f1b2d662a2fc33ff6c9472d698bfdcdad7288
Mohamed Karim Boughammoura <boughammoura.karim@gmail.com>
2019-04-10 01:02:24 +0200

    Ajoutez des fichiers projet.

---
+++ rasa_nlu_db_storage/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using rasa_nlu_db_storage.Models;
+
+namespace rasa_nlu_db_storage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ rasa_nlu_db_storage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace rasa_nlu_db_storage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
